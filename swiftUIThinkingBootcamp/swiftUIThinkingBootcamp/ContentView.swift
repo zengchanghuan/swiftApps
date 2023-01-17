93c7c0cdda7675efa824982ea2a341f72e244e51
ZengChanghuan <zengchanghuan@gmail.com>
2023-01-17 22:26:18 +0800

    add project

---
+++ swiftUIThinkingBootcamp/swiftUIThinkingBootcamp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIThinkingBootcamp
+//
+//  Created by ZengChanghuan on 2023/1/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIThinkingBootcamp/swiftUIThinkingBootcamp/swiftUIThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIThinkingBootcampApp.swift
+//  swiftUIThinkingBootcamp
+//
+//  Created by ZengChanghuan on 2023/1/17.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
